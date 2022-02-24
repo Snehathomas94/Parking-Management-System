52472e87770ef403937eb0a9ea5bab0d9a2b1390
Snehathomas94 <70622642+Snehathomas94@users.noreply.github.com>
2022-02-24 18:18:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 
 const app = express();
+app.use(bodyParser.json());
 
 
 // app.get('/', (req,res) => {
