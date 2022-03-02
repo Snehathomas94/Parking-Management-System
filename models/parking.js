ca4c34784ce650c2dff8a2543133c661277c2a89
Snehathomas94 <70622642+Snehathomas94@users.noreply.github.com>
2022-03-02 16:27:22 +0530

    Update parking.js

--- models/parking.js
+++ models/parking.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
+let parkingSchema = new Schema(
+    {
+        
+        vehicleNumber: {type:String},
+        parkingSlot : {type:String},
+        entryTime :{type:String},
+        exitTime : {type:String},
+        date: { type: String},
+        fare : {type:Number},
+        parkingAlloted : {type:Boolean}
+    }
+);
+
+module.exports = mongoose.model("parking", parkingSchema);
